f8ea4dfa2b6d0d65ef5ff53e90de749f214c3272
DESKTOP-B39AMRF\Ibrahim <ibrahim.boudagga.cg@gmail.com>
2025-08-16 00:31:43 +0100

    Add SignInModel for handling sign-in responses and remove unused use cases. Introduce sign-in navigation use case for improved user flow.

--- lib/src/features/auth/1_sign_in/vm/usecases/create_account_uc.dart
+++
@@ -1,7 +0,0 @@
-part of '../bloc/sign_in_bloc.dart';
-
-extension on SignInBloc {
-  void _createAccount(CreateAccount event, Emitter<SignInState> emit) async {
-    //TODO: Implement create account
-  }
-}
--- lib/src/features/auth/1_sign_in/vm/usecases/forgot_password_uc.dart
+++
@@ -1,7 +0,0 @@
-part of '../bloc/sign_in_bloc.dart';
-
-extension ForgotPasswordUC on SignInBloc {
-  Future<void> _forgotPassword(ForgotPassword event, Emitter<SignInState> emit) async {
-    //!Navigate to forgot password screen (GoRouter)
-  }
-}
