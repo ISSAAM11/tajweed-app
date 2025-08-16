0629b710e50011e5f83ad0b3970f19be0793a767
DESKTOP-B39AMRF\Ibrahim <ibrahim.boudagga.cg@gmail.com>
2025-08-16 14:05:57 +0100

    Update app router to streamline navigation by replacing direct reference to SignInScreen with signInRoute. Import necessary authentication features for improved routing capabilities.

--- lib/src/app/router/app_router.dart
+++ lib/src/app/router/app_router.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:tajweed_ai/src/features/auth/export.dart';
 
-import '../../features/auth/1_sign_in/router/sign_in_route.dart';
-import '../../features/auth/1_sign_in/view/screen/sign_in_screen.dart';
 
 final router = GoRouter(
   navigatorKey: GlobalKey<NavigatorState>(debugLabel: 'root'),
-  initialLocation: SignInScreen.path,
+  initialLocation: signInRoute.path,
   debugLogDiagnostics: true,
   routes: [
     signInRoute,
