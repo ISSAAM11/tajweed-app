beb3af4f35d1dbc7664daa5d95596f7ebc6254ff
DESKTOP-B39AMRF\Ibrahim <ibrahim.boudagga.cg@gmail.com>
2025-08-23 12:25:40 +0100

    Enhance AppConfig to support asynchronous initial route setting and token management
    - Updated setInitialRoute method to be asynchronous, allowing for dynamic route determination based on cached tokens.
    - Introduced _setFirstRoute method to retrieve access and refresh tokens from CacheManager, improving flow control for user authentication.
    - Added error handling to ensure fallback to the sign-in route in case of token retrieval failure.

--- lib/src/app/config/app_config.dart
+++ lib/src/app/config/app_config.dart
@@ -1,4 +1,8 @@
-import '../../app/environment/app_environment.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../../core/managers/cache/cache_manager_impl.dart';
+import '../../app/binding/app_bindings.dart';
+import 'package:cg_core_defs/cg_core_defs.dart';
+
 import '../../features/auth/export.dart';
 
 abstract final class AppConfig {
@@ -6,11 +10,27 @@ abstract final class AppConfig {
 
   static String get initialRoute => _initialRoute;
 
-  static void setInitialRoute([String? devRoute]) {
-    _initialRoute = AppEnvironment.current == Environment.dev
-        ? devRoute ?? signInRoute.path
-        : _getFirstRoute();
+  static Future<void> setInitialRoute([String? devRoute]) async {
+    _initialRoute = devRoute ?? await _setFirstRoute();
   }
 
-  static String _getFirstRoute() => signInRoute.path;
+  static Future<String> _setFirstRoute() async {
+    try {
+      final cacheManager =
+          get<CacheManager<SharedPreferences>>() as CacheManagerImpl;
+      final [accessToken, refreshToken] = await Future.wait([
+        cacheManager.getSecureString('access_token'),
+        cacheManager.getSecureString('refresh_token'),
+      ]);
+      Debugger.red(accessToken);
+      Debugger.green(refreshToken);
+
+      return (accessToken != null && refreshToken != null)
+          ? signUpRoute.path
+          : signInRoute.path;
+    } catch (e) {
+      Debugger.red(e.toString());
+      return signInRoute.path;
+    }
+  }
 }
